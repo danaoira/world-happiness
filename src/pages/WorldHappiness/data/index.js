62393f6c52dd3d75a3d2b22f2f66bdaf7192e418
Dana Toribio <dana.toribio@noodle.ai>
2019-07-21 03:42:52 -0700

    Working data ingestion

---
+++ src/pages/WorldHappiness/data/index.js
@@ -0,0 +1,7 @@
+import data from './world-happiness-report-2019.csv'
+import serialize from './serialize'
+
+export {
+  data,
+  serialize
+}
--- src/pages/WorldHappiness/view.js
+++ src/pages/WorldHappiness/view.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import { Title } from '../../styles/content'
+import { csv } from 'd3-fetch'
+import { data, serialize } from './data'
 
 class WorldHappiness extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    csv(data, d => serialize(d)).then(d => this.setState({ data: d }))
+  }
 
   render() {
     return (
-      <Title>World Happiness Report</Title>
+      <React.Fragment>
+        <Title>World Happiness Report</Title>
+        {this.state.data && this.state.data.map((d, i) => 
+          <div key={i}>{JSON.stringify(d)}</div>
+        )}
+      </React.Fragment>
     )
   }
 }
-
 
 export default WorldHappiness
