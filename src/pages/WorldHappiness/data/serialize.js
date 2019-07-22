a6211a24da1aa316d4d0ae0ddb25cab33e8029a0
Dana Toribio <dana.toribio@noodle.ai>
2019-07-22 11:13:51 -0700

    WIP graph is rendered but wrong xD

--- src/pages/WorldHappiness/data/serialize.js
+++ src/pages/WorldHappiness/data/serialize.js
@@ -1,15 +1,15 @@
 const serialize = (d) => ({
   country: d['Country (region)'],
-  ladder: d['Ladder'],
-  ladderSD: d['SD of Ladder'],
-  positiveAffect: d['Positive affect'],
-  negativeAffect: d['Negative affect'],
-  socialSupport: d['Social support'],
-  freedom: d['Freedom'],
-  corruption: d['Corruption'],
-  generosity: d['Generosity'],
-  gdpPerCapita: d['Log of GDP per capita'],
-  lifeExpectancy: d['Healthy life expectancy']
+  ladder: +d['Ladder'],
+  ladderSD: +d['SD of Ladder'],
+  positiveAffect: +d['Positive affect'],
+  negativeAffect: +d['Negative affect'],
+  socialSupport: +d['Social support'],
+  freedom: +d['Freedom'],
+  corruption: +d['Corruption'],
+  generosity: +d['Generosity'],
+  gdpPerCapita: +d['Log of GDP per capita'],
+  lifeExpectancy: +d['Healthy life expectancy']
 })
 
 export default serialize
--- src/pages/WorldHappiness/view.js
+++ src/pages/WorldHappiness/view.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react'
 import { Title } from '../../styles/content'
 import { csv } from 'd3-fetch'
+import { scaleBand, scaleLinear } from 'd3-scale'
+import { extent } from 'd3-array'
 import { data, serialize } from './data'
+import { theme } from '../../utils'
+
+const svg = {
+  width: 900,
+  height: 360
+}
+
+const BarChart = ({ data, selectedKey }) => {
+  const xScale = scaleBand()
+    .domain([0, data.length])
+    .range([0, svg.width])
+    .padding(10)
+
+  const yScale = scaleLinear()
+    .domain(extent(data.map(d => d[selectedKey])))
+    .range([svg.height, 0])
+
+  return (
+    <svg width={svg.width} height={svg.height}>
+      {data.map((d, i)=> {
+        return (
+        <React.Fragment>
+          <rect
+            className={d.country}
+            key={`${d}-${i}`}
+            y={0}
+            x={yScale(d[selectedKey])}
+            width={1}
+            height={yScale(d[selectedKey])}
+            fill={theme.color.pink}
+          />
+          {/* <text x={10} y={10}>{d.country}</text> */}
+        </React.Fragment>
+      )})}
+    </svg>
+  )
+}
+  
+/**
+ * Sort data by ascending or descending key
+ * @param {Array}   data  array of data
+ * @param {String}  key   key name
+ * @param {string}  sort  sort, default desc
+*/
+const getDataSortedByKey = (data, key, sort = 'desc') => (data
+  .sort((a, b) => sort === 'desc' 
+    ? b[key] - a[key] 
+    : a[key] - b[key]
+  )
+)
 
 class WorldHappiness extends Component {
   constructor(props) {
@@ -12,16 +64,22 @@ class WorldHappiness extends Component {
   }
 
   componentDidMount() {
+    /** data ingestion */
     csv(data, d => serialize(d)).then(d => this.setState({ data: d }))
   }
 
   render() {
+    if (!this.state.data) {
+      return null
+    }
+
+    const sortedData = getDataSortedByKey(this.state.data, 'generosity')
+
     return (
       <React.Fragment>
         <Title>World Happiness Report</Title>
-        {this.state.data && this.state.data.map((d, i) => 
-          <div key={i}>{JSON.stringify(d)}</div>
-        )}
+        <BarChart data={sortedData} selectedKey={'generosity'} />
+        {sortedData.map((d, i) => (<div key={i}>{JSON.stringify(d)}</div>))}
       </React.Fragment>
     )
   }
